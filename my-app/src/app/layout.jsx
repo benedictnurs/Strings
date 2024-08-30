43c82e640fd448046f664ed878e1c76aa864fb93
benedictnurs <benedictnursalim@gmail.com>
2024-08-29 22:07:28 -0700

    Started Clerk will do DB soon.

--- my-app/src/app/layout.jsx
+++ my-app/src/app/layout.jsx
@@ -1,6 +1,13 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import StoreProvider from './StoreProvider';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,14 +18,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <StoreProvider>
-      <html lang="en">
-        <head>
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-        </head>
-        <body className={inter.className}>{children}</body>
-      </html>
-    </StoreProvider>
+    <ClerkProvider>
+      <StoreProvider>
+        <html lang="en">
+          <head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+          </head>
+          <body className={inter.className}>{children}</body>
+        </html>
+      </StoreProvider>
+    </ClerkProvider>
+
   );
 }
